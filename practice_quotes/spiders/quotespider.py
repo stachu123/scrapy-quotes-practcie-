d5ee8b911128225bb96896d14ce112c266348463
Stanislaw Zadroga <stanisawzdrg@gmail.com>
2023-10-09 19:19:50 +0200

    first commit

---
+++ practice_quotes/spiders/quotespider.py
@@ -0,0 +1,10 @@
+import scrapy
+
+
+class QuotespiderSpider(scrapy.Spider):
+    name = "quotespider"
+    allowed_domains = ["quotes.toscrape.com"]
+    start_urls = ["https://quotes.toscrape.com"]
+
+    def parse(self, response):
+        pass
